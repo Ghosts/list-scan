d3f51fd1da431c8b4ac9d29af0fc790c965bb086
Caden Sumner <cadenjsumner@gmail.com>
2018-10-22 18:33:55 -0400

    Let's use react well

--- components/Scanner.js
+++ components/Scanner.js
@@ -5,8 +5,6 @@ const Scanner = () => (
     <div>
         <Head>
             <script src='/static/jquery.js'></script>
-            <script src='/static/quagga.min.js'></script>
-            <script src='/static/scanner.js'></script>
             <link rel="stylesheet" type="text/css" href="/static/scanner.css" />
         </Head>
         <section className="hero is-light max-height">
