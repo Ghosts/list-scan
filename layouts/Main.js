7f10af4b3fe6e51be7a5967e57c41eacdb91bc40
Caden Sumner <cadenjsumner@gmail.com>
2018-10-22 13:03:49 -0400

    Test code specification & fixes to style

--- layouts/Main.js
+++ layouts/Main.js
@@ -1,14 +1,23 @@
 import Scanner from '../components/Scanner'
 import Head from 'next/head'
 
+const layoutStyle = {
+    margin: 0,
+    padding: 0,
+
+}
+
 const Layout = (props) => (
-    <div>
+    <div style={layoutStyle} className="columns">
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="/static/bulma.min.css" type="text/stylesheet"/>
     </Head>
+    <div className="column">
         <h1>List-Scan</h1>
         <Scanner />
         {props.children}
+        </div>
     </div>
 )
 
--- static/scanner.js
+++ static/scanner.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    let codes = [];
+
     var liveStreamConfig = {
         inputStream: {
             name: "Live",
@@ -12,18 +14,11 @@ $(document).ready(function() {
             patchSize: "medium",
             halfSample: true
         },
-        numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+        numOfWorkers: (window.navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
         decoder: {
             readers: [
-                "code_128_reader",
-                "ean_reader",
-                "ean_8_reader",
                 "code_39_reader",
                 "code_39_vin_reader",
-                "codabar_reader",
-                "upc_reader",
-                "upc_e_reader",
-                "i2of5_reader"
             ],
         },
         locate: true
@@ -41,35 +36,45 @@ $(document).ready(function() {
         }
     );
 
-    Quagga.onProcessed(function (result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
+    // Quagga.onProcessed(function (result) {
+    //     var drawingCtx = Quagga.canvas.ctx.overlay,
+    //         drawingCanvas = Quagga.canvas.dom.overlay;
 
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-                });
-            }
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-            }
+    //     if (result) {
+    //         if (result.boxes) {
+    //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+    //             result.boxes.filter(function (box) {
+    //                 return box !== result.box;
+    //             }).forEach(function (box) {
+    //                 Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+    //             });
+    //         }
+    //         if (result.box) {
+    //             Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+    //         }
 
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-            }
-        }
-    });
+    //         if (result.codeResult && result.codeResult.code) {
+    //             Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+    //         }
+    //     }
+    // });
 
     Quagga.onDetected(function (result) {
-        if (result.codeResult.code) {
-            $("#scanned").append(`<li>${result.codeResult.code}</li>`);
-            if ("vibrate" in navigator) {
-                navigator.vibrate(1000);
-            }
+        let code = result.codeResult.code;
+        if (result.codeResult.code && !$.inArray(code, codes)) {
+            codes.push(code);
+            $("#scanned").append(`
+            <span class="tag is-success">
+            ${code}
+            <button onClick="removeCode('${code}')" class="delete is-small"></button>
+            </span>
+            `);
+            window.navigator.vibrate(1000);
         }
     });
+
+    function removeCode(code) {
+        codes.splice( $.inArray(code, codes), 1 );
+    }
+
 });
