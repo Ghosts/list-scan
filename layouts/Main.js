749f06f82a65dd0b3b809c06af33ee1e48c4208c
Caden Sumner <cadenjsumner@gmail.com>
2018-10-22 13:35:36 -0400

    overflow fix

--- layouts/Main.js
+++ layouts/Main.js
@@ -1,13 +1,15 @@
 import Header from '../components/Header'
 import Head from 'next/head'
 
-const layoutStyle = {
-    margin: 0,
-    padding: 0,
-}
-
 const Layout = (props) => (
     <div>
+
+    <style jsx global>{`
+      body { 
+        overflow-x: hidden;
+        overflow-y: hidden;
+      }
+    `}</style>
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="stylesheet" type="text/css" href="/static/bulma.min.css"/>
