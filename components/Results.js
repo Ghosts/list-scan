1e58ef0f59dc7dd1da04c24ae7dcef60c96d6ab6
Caden Sumner <cadenjsumner@gmail.com>
2018-10-23 17:10:57 -0400

    Moving around components & figuring out the binding

--- components/Results.js
+++ components/Results.js
@@ -1,99 +1,16 @@
 import React from 'react'
-import Quagga from 'quagga'
 
 class Results extends React.Component {
-    codes = [];
 
     constructor(props) {
         super(props);
-        this.scanned_codes = React.createRef();
     }
 
-    componentDidMount() {
-        var liveStreamConfig = {
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                constraints: {
-                    facingMode: "environment",
-                    width: window.innerWidth,
-                    height: window.innerHeight,
-                },
-                target: document.querySelector('#barcode-scanner')
-            },
-            locator: {
-                patchSize: "medium",
-                halfSample: true
-            },
-            area: {
-                top: "0%",
-                right: "0%",
-                left: "0%",
-                bottom: "0%"
-            },
-            numOfWorkers: (window.navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
-            decoder: {
-                readers: ["code_39_reader", "code_39_vin_reader", "code_128_reader"]
-            },
-            locate: true,
-            multiple: true
-        };
-
-        Quagga.init(
-            liveStreamConfig,
-            function (err) {
-                if (err) {
-                    console.log(err);
-                    Quagga.stop();
-                    return;
-                }
-                Quagga.start();
-            }
-        );
-
-        Quagga.onProcessed(function (result) {
-            var drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
-            if (result) {
-                if (result.boxes) {
-                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                    result.boxes.filter(function (box) {
-                        return box !== result.box;
-                    }).forEach(function (box) {
-                        Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-                    });
-                }
-
-                if (result.box) {
-                    Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-                }
-
-                if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-                }
-            }
-        });
-
-        Quagga.onDetected(function (result) {
-            let code = result.codeResult.code;
-            if (code && ($.inArray(code, codes) === -1)) {
-                codes.push(code);
-                document.querySelector("#scanned-codes").appendChild(`
-                    <span class="tag is-light is-large ${code}">
-                    ${code}
-                    <button onClick="removeCode('${code}')" class="delete"></button>
-                    </span>
-                    `);
-                window.navigator.vibrate(500);
-            }
-        });
-
-        function removeCode(code) {
-            var i = codes.indexOf(code);
-            if (i != -1) {
-                codes.splice(i, 1);
-            }
-            document.querySelector(`.${code}`).remove();
+    AddResult(result) {
+        let code = result.codeResult.code;
+        if (code && ($.inArray(code, this.props.codes) === -1)) {
+            this.props.codes.push(code);
+            window.navigator.vibrate(500);
         }
     }
 
@@ -105,7 +22,7 @@ class Results extends React.Component {
                         <h1 className="title">
                             Scanned Codes
                         </h1>
-                        <div id="scanned-codes" ref={this.scanned_codes} className="tags"></div>
+                        <ScannedCodes id="scanned_codes" ref={this.scanned_codes} codes={this.props.codes} className="tags" />
                         <div className="subtitle remove-all">
                             <a className="button is-rounded" onClick={this.removeAll.bind(this)}>Remove All</a>
                         </div>
@@ -115,12 +32,41 @@ class Results extends React.Component {
         )
     }
     removeAll() {
-        this.codes = [];
+        this.props.codes = [];
         const results = this.scanned_codes.curent;
         while (results.hasChildNodes()) {
             results.removeChild(results.lastChild);
         }
     }
+}
+
+class ScannedCodes extends React.Component {
+
+    removeCode(code) {
+        var i = this.props.codes.indexOf(code);
+        if (i != -1) {
+            this.props.codes.splice(i, 1);
+        }
+        document.querySelector(`.${code}`).remove();
+    }
+
+    render() {
+        const codes = this.props.codes;
+        return (
+            <div>
+            {
+                codes.map(function(code, idx) {
+                    return (
+                        <span className={`tag is-light is-large ${code}`}>
+                        {code}
+                        <button onClick={removeCode(code)} className="delete"></button>
+                        </span>
+                      )
+                })
+            }
+            </div>
+         )
+       }
 }
 
 
--- components/Scanner.js
+++ components/Scanner.js
@@ -1,21 +1,115 @@
 import Head from 'next/head'
+import Quagga from 'quagga'
 import Results from './Results'
 
-const Scanner = () => (
-    <div>
-        <Head>
-            <script src='/static/jquery.js'></script>
-            <link rel="stylesheet" type="text/css" href="/static/scanner.css" />
-        </Head>
-        <section className="hero is-light max-height">
-            <div className="hero-body">
-                <div className="container">
-                    <div id="barcode-scanner" className="viewport"></div>
+class Scanner extends React.Component {
+    codes = [];
+
+    constructor(props) {
+        super(props);
+        this.results = React.createRef();
+    }
+
+    render() {
+        return (<div>
+            <Head>
+                <script src='/static/jquery.js'></script>
+                <link rel="stylesheet" type="text/css" href="/static/scanner.css" />
+            </Head>
+            <section className="hero is-light max-height">
+                <div className="hero-body">
+                    <div className="container">
+                        <div id="barcode-scanner" className="viewport"></div>
+                    </div>
                 </div>
-            </div>
-        </section>
-        <Results />
-    </div>
-)
+            </section>
+            <Results codes={this.codes} ref={this.results} />
+        </div>
+        )
+    }
+
+    // Quagga code
+    onProcessed() {
+        Quagga.onProcessed(function (result) {
+            var drawingCtx = Quagga.canvas.ctx.overlay,
+                drawingCanvas = Quagga.canvas.dom.overlay;
+            if (result) {
+                if (result.boxes) {
+                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                    result.boxes.filter(function (box) {
+                        return box !== result.box;
+                    }).forEach(function (box) {
+                        Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+                    });
+                }
+
+                if (result.box) {
+                    Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+                }
+
+                if (result.codeResult && result.codeResult.code) {
+                    Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+                }
+            }
+        });
+    }
+
+    onDetected() {
+        var resultRef = this.results.current;
+        Quagga.onDetected(function (result) {
+            resultRef.AddResult(result);
+        });
+    }
+
+    init(liveStreamConfig) {
+        Quagga.init(
+            liveStreamConfig,
+            function (err) {
+                if (err) {
+                    console.log(err);
+                    Quagga.stop();
+                    return;
+                }
+                Quagga.start();
+            }
+        );
+    }
+
+
+    componentDidMount() {
+        var liveStreamConfig = {
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                constraints: {
+                    facingMode: "environment",
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                },
+                target: document.querySelector('#barcode-scanner')
+            },
+            locator: {
+                patchSize: "medium",
+                halfSample: true
+            },
+            area: {
+                top: "0%",
+                right: "0%",
+                left: "0%",
+                bottom: "0%"
+            },
+            numOfWorkers: (window.navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+            decoder: {
+                readers: ["code_39_reader", "code_39_vin_reader", "code_128_reader"]
+            },
+            locate: true,
+            multiple: true
+        }
+
+        this.init(liveStreamConfig)
+        this.onDetected()
+        this.onProcessed()
+    }
+}
 
 export default Scanner
