d971655702b9645a4194c4e8909af356418e144c
Caden Sumner <cadenjsumner@gmail.com>
2018-10-23 16:04:15 -0400

    Maybe fix export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    exportPathMap: function () {
-        return {
-          "/": { page: "/" },
-        }
-      },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/{list-scan}' : '',
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
   }
+}
