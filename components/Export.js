a135e559129fcd928192e2e7f3a0b7bdfd0548e2
Caden Sumner <cadenjsumner@gmail.com>
2018-10-24 09:57:51 -0400

    Test remove code changes

--- components/Export.js
+++ components/Export.js
@@ -13,8 +13,8 @@ class Export extends React.Component {
                     <div className="container">
                         <h1 className="title has-text-centered is-bold">
                             Export
+                            <small>There are {this.props.codes.length} scanned codes.</small>
                         </h1>
-                        <small>There are {this.props.codes.length} scanned codes.</small>
                         <div className="buttons is-centered">
                             <CSVLink className="button is-warning is-rounded" data={this.props.codes} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink>
                             <a href={`mailto:?subject=list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}&body=${this.props.codes.toString().split(',').join("<br/>")}`} className="button is-warning is-rounded">Email</a>
--- components/Results.js
+++ components/Results.js
@@ -16,18 +16,18 @@ class Results extends React.Component {
                         <div className="container has-text-centered">
                             <h1 className="title">
                                 Scanned Codes
-                        </h1>
+                            </h1>
                             <div className="tags" ref={this.scanned_codes}>
                                 {
                                     this.props.codes.map((code) => (
-                                        <span className={`tag is-light is-large ${code}`}>
+                                        <span className="tag is-light is-large">
                                             {code}
                                             <button onClick={this.removeCode.bind(this, code)} className="delete"></button>
                                         </span>
                                     ))
                                 }
                             </div>
-                            {this.props.codes.length == 0 ? "Scanned Codes will show up here." :
+                            {this.props.codes.length == 0 ? "Scanned codes will show up here." :
                                 <div className="subtitle remove-all">
                                     <button className="button is-light is-rounded" onClick={this.removeAll.bind(this)}>Remove All</button>
                                 </div>
@@ -41,28 +41,29 @@ class Results extends React.Component {
     }
 
     addResult(result) {
-        let code = result.codeResult.code;
+        let code = result.codeResult.code
         if (code && ($.inArray(code, this.props.codes) === -1)) {
-            let new_codes = this.props.codes;
-            new_codes.push(code);
+            let new_codes = this.props.codes
+            new_codes.push(code)
             this.setState({
                 codes: new_codes
             });
-            window.navigator.vibrate(500);
+            window.navigator.vibrate(500)
         }
     }
 
     removeAll() {
+        let new_codes = []
         this.setState({
-            codes: new []
+            codes: new_codes
         });
     }
 
     removeCode(code) {
-        let new_codes = this.props.codes;
-        var i = new_codes.indexOf(code);
+        let new_codes = this.props.codes
+        var i = new_codes.indexOf(code)
         if (i != -1) {
-            new_codes.splice(i, 1);
+            new_codes.splice(i, 1)
         }
         this.setState({
             codes: new_codes
