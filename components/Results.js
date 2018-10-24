839e02a3c0bd8c7af222ac84f56e8cae81494c8e
Caden Sumner <cadenjsumner@gmail.com>
2018-10-24 10:23:01 -0400

    UNFO

--- components/Results.js
+++ components/Results.js
@@ -61,7 +61,8 @@ class Results extends React.Component {
 
     removeCode(code) {
         let new_codes = this.props.codes;
-        if (new_codes.indexOf(code) != -1) {
+        var i = new_codes.indexOf(code);
+        if (i != -1) {
             new_codes.splice(i, 1);
         }
         this.setState({
