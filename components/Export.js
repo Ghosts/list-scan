cf5c6184e2009c6f4127648788125c10f769faa7
Caden Sumner <cadenjsumner@gmail.com>
2018-10-24 10:17:31 -0400

    Move subtitle back

--- components/Export.js
+++ components/Export.js
@@ -14,8 +14,8 @@ class Export extends React.Component {
                         <h1 className="title has-text-centered is-bold">
                             Export
                             <br/>
+                            <small className="subtitle has-text-centered">There are {this.props.codes.length} scanned codes.</small>
                         </h1>
-                        <small className="subtitle has-text-centered">There are {this.props.codes.length} scanned codes.</small>
                         <div className="buttons is-centered">
                             <CSVLink className="button is-warning is-rounded" data={this.props.codes} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink>
                             <a href={`mailto:?subject=list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}&body=${this.props.codes.toString().split(',').join("<br/>")}`} className="button is-warning is-rounded">Email</a>
