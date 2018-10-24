23a2732185a7810eef3adf239a6eec8259990610
Caden Sumner <cadenjsumner@gmail.com>
2018-10-24 19:52:03 -0400

    Update SW

--- static/sw.js
+++ static/sw.js
@@ -1,14 +1,61 @@
-const CACHE_NAME = "cache";
-const urlsToCache = ["/"];
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", event => {
-    const preLoaded = caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
-    event.waitUntil(preLoaded);
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.js',
+  './', // Alias for index.html
+  '/static/bulma.min.css',
+  '/static/scanner.css'
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
 });
 
-self.addEventListener("fetch", event => {
-    const response = caches.match(event.request)
-        .then(match => match || fetch(event.request));
-    event.respondWith(response);
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
 });
