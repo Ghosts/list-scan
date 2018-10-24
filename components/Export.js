ca1b2135c72427c197d5e22497d4f581a844e500
Caden Sumner <cadenjsumner@gmail.com>
2018-10-24 09:44:09 -0400

    Test

--- components/Export.js
+++ components/Export.js
@@ -5,6 +5,7 @@ class Export extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            codes: this.props.codes,
             mailto: `mailto:?subject=list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}&body=${this.props.codes.toString().split(',').join("<br/>")}`
         }
     }
@@ -16,10 +17,10 @@ class Export extends React.Component {
                     <div className="container">
                         <h1 className="title has-text-centered is-bold">
                             Export
-                            <div>Scanned: {this.props.codes}</div>
+                            <div>Scanned: {this.state.codes.length}</div>
                         </h1>
                         <div className="buttons is-centered">
-                            <CSVLink className="button is-warning is-rounded" data={this.props.codes} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink>
+                            <CSVLink className="button is-warning is-rounded" data={this.state.codes} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink>
                             <a href={this.state.mailto} className="button is-warning is-rounded">Email</a>
                         </div>
                     </div>
