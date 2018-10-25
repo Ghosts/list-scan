cf4fdbfd6bda6dd6ce3f97ecfde173948412adc3
Caden Sumner <cadenjsumner@gmail.com>
2018-10-25 17:58:30 -0400

    Hide CSV until fixed

--- components/Export.js
+++ components/Export.js
@@ -17,7 +17,7 @@ class Export extends React.Component {
                             <small className="subtitle has-text-centered">There are {this.props.codes.length} scanned codes.</small>
                         </h1>
                         <div className="buttons is-centered">
-                            <CSVLink className="button is-warning is-rounded" data={this.props.codes} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink>
+                            {/* <CSVLink className="button is-warning is-rounded" data={this.props.codes.toString()} filename={`list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}.csv`}>Download CSV</CSVLink> */}
                             <a href={`mailto:?subject=list-scan-${new Date().toLocaleDateString().replace(/\//g, '-')}&body=${this.props.codes.toString().split(',').join("%0D%0A")}`} className="button is-warning is-rounded">Email</a>
                         </div>
                     </div>
